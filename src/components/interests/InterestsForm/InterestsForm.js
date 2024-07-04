8f0d18c8d9150935169de8d55aaa21ef2a9870be
RamunnoAJ <ramunnoagustin@gmail.com>
2024-07-04 19:09:11 -0300

    Creo componente para el formulario de intereses

---
+++ src/components/interests/InterestsForm/InterestsForm.js
@@ -0,0 +1,32 @@
+import styles from "./InterestsForm.module.css";
+
+/**
+ * @component
+ * @param {Object} param0
+ * @param {function} param0.onSubmit
+ * @param {function} param0.onChange
+ * @param {string} param0.value
+ * @param {placeholder} param0.placeholder
+ * @returns {JSX.Element}
+ */
+export default function InterestsForm({
+  onSubmit,
+  onChange,
+  value,
+  placeholder,
+}) {
+  return (
+    <form onSubmit={onSubmit} className={styles.form}>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className={styles.input}
+      />
+      <button type="submit" className={styles.button}>
+        Agregar
+      </button>
+    </form>
+  );
+}
