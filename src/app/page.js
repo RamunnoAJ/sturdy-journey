6528a672c2f0e41775e159e9eafb630102db3a0c
RamunnoAJ <ramunnoagustin@gmail.com>
2024-07-04 19:09:18 -0300

    Creo página de intereses

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,71 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import InterestsItem from "@/components/interests/InterestsItem/InterestsItem";
+import InterestsForm from "@/components/interests/InterestsForm/InterestsForm";
+
 export default function Home() {
-  return <main></main>;
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem("items")));
+  const [newItem, setNewItem] = useState("");
+
+  function addItem() {
+    if (newItem.trim() !== "") {
+      setItems([...items, newItem]);
+      setNewItem("");
+    }
+  }
+
+  function deleteItem(index) {
+    const newItems = items.filter((_, i) => i !== index);
+    setItems(newItems);
+  }
+
+  function editItem(index, newValue) {
+    const newItems = items.map((item, i) => (i === index ? newValue : item));
+
+    setItems(newItems);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addItem();
+  }
+
+  useEffect(() => {
+    const savedItems = JSON.parse(localStorage.getItem("items"));
+    if (savedItems) {
+      setItems(savedItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.container}>
+        <h2 className={styles.title}>Intereses</h2>
+        <InterestsForm
+          onSubmit={handleSubmit}
+          onChange={setNewItem}
+          value={newItem}
+          placeholder={"Nuevo interés"}
+        />
+        <ul className={styles.list}>
+          {items.map((item, index) => (
+            <InterestsItem
+              index={index}
+              onChange={editItem}
+              onDelete={deleteItem}
+              value={item}
+              key={index}
+            />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
